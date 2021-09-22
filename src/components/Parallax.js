324b824d32cebb624eb2491db96e27370c2077d8
urgent <urgent.programmer@gmail.com>
2021-09-21 21:53:20 -0400

    Basic demo

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,22 +1,34 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const Parallax = ({ children }) => {
+const Parallax = ({ images, anchors, children }) => {
+
+    gsap.registerPlugin(ScrollTrigger);
+    const ref = useRef(null);
+
     useEffect(() => {
-        const onScroll = e => {
-            setScrollTop(e.target.documentElement.scrollTop);
-            setScrolling(e.target.documentElement.scrollTop > scrollTop);
-        };
-        window.addEventListener("scroll", onScroll);
-
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [scrollTop]);
+        const element = ref.current;
+        let tl = gsap.timeline();
+        images.forEach((image, index) => {
+            tl.to(".parallax-content img", {
+                attr: { src: image },
+                scrollTrigger: {
+                    trigger: element.querySelector(anchors[index]),
+                    start: "top top",
+                    end: "bottom center",
+                    scrub: true
+                }
+            })
+        })
+    }, [images, anchors]);
 
     return (
-        <div className=" parallax-container" id="parallax">
+        <div className=" parallax-container" id="parallax" ref={ref}>
             <div className="parallax-content">
                 <div style={{ position: "sticky", right: "0", top: "0" }}>
                     <div style={{ position: "absolute", right: "0", top: "15rem" }} >
-                        <img src="https://cataas.com/cat" style={{ maxWidth: "200px" }} />
+                        <img src="https://cataas.com/cat?b" style={{ maxWidth: "200px" }} />
                     </div>
                 </div>
                 <div style={{ maxWidth: "780px" }}> {children} </div>
